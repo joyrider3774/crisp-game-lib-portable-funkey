2dad2da1e982571813c529c2b5fb53f743ddfa26
willems davy <joyrider3774@hotmail.com>
2025-02-13 16:48:12 +0100

    fix scores, overlay settings not loading with direct game launch and allow fake roms for direct game launch

--- package.sh
+++ package.sh
@@ -7,6 +7,7 @@ mkdir -p opk
 cp cglpsdl1 opk/cglpsdl1
 cp meta/cglpsdl1.png opk/cglpsdl1.png
 cp meta/Cglpsdl1.funkey-s.desktop opk/Cglpsdl1.funkey-s.desktop
+cp meta/explorer.desktop opk/explorer.desktop
 
 mksquashfs ./opk Cglpsdl1.opk -all-root -noappend -no-exports -no-xattrs
 
--- src/cglpSDL1.c
+++ src/cglpSDL1.c
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <string.h>
 #include "machineDependent.h"
 #include "cglp.h"
 #include "trig_normalization.h"
@@ -67,7 +68,7 @@ static bool glowEnabled = DEFAULT_GLOWENABLED;
 static Uint32 videoFlags = SDL_SWSURFACE;
 static bool nodelay = false;
 static SDL_AudioSpec audiospec = {0};
-static int startgame = -1;
+static char startgame[100] = {0};
 
 static int fpsSamples[FPS_SAMPLES];
 static bool showfps = false;
@@ -1060,7 +1061,7 @@ static void update()
 
     if ((prevKeys[BUTTON_MENU] == 0) && (keys[BUTTON_MENU] == 1))
     {
-        if (!isInMenu && (startgame == -1))
+        if (!isInMenu && (startgame[0] == 0))
             goToMenu();
         else
             quit = 1;
@@ -1243,18 +1244,70 @@ static void printHelp(char* exe)
     printf("  -ms: Make screenshot of every game\n");
 }
 
+void SDL_Cleanup()
+{
+    SDL_Event Event;
+    while(SDL_PollEvent(&Event))
+        SDL_Delay(1);
+    SDL_Delay(250);
+    
+    SDL_Quit();
+}
+
 int main(int argc, char **argv)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) == 0)
 	{
+        atexit(SDL_Cleanup);
         printf("SDL Succesfully initialized\n");
 		bool fullScreen = false;
         bool useHWSurface = false;
         bool noAudioInit = false;
         bool makescreenshots = false;
-		for (int i=0; i < argc; i++)
+        for (int i=0; i < argc; i++)
 		{
-        	if((strcmp(argv[i], "-?") == 0) || (strcmp(argv[i], "--?") == 0) || 
+            printf("param %d %s\n", i, argv[i]);
+            char *ext = strrchr(argv[i], '.');
+            if(ext != NULL)
+            {
+                if(strcmp(ext, ".cgl") == 0)
+                {
+                    memset(startgame, 0, 100);
+                    char *gamestart = strrchr(argv[i], '/');
+                    if(gamestart == NULL)
+                        gamestart = strrchr(argv[i], '\\');
+                    if(gamestart != NULL)
+                    {
+                        gamestart++;
+                        for(char* j = gamestart; j < ext; j++)
+                            startgame[j-gamestart] = toupper(*j);
+                    }
+                }
+            }
+            
+            if(strcmp(argv[i], "-cgl") == 0)
+            {
+                initGame();
+                for(int i = 0; i < gameCount; i++)
+                {
+                    Game g = getGame(i);
+                    if(strlen(g.title) > 0 && (g.update != NULL))
+                    {
+                        
+                        char filename[512];
+                        sprintf(filename, "./%s.cgl", g.title);
+                        FILE *f = fopen(filename, "w");
+                        if(f)
+                        {
+                            fwrite(g.title, sizeof(char), strlen(g.title), f);
+                            fclose(f);
+                        }
+                    }
+                }
+                return 0;
+            }
+
+            if((strcmp(argv[i], "-?") == 0) || (strcmp(argv[i], "--?") == 0) || 
             	(strcmp(argv[i], "/?") == 0) || (strcmp(argv[i], "-help") == 0) || (strcmp(argv[i], "--help") == 0))
         	{
                 printHelp(argv[0]);
@@ -1286,7 +1339,10 @@ int main(int argc, char **argv)
             
             if(strcmp(argv[i], "-g") == 0)
                 if(i+1 < argc)
-                    startgame = i+1;
+                {
+                    memset(startgame, 0, 100);
+                    strcpy(startgame, argv[i+1]);
+                }
             
             if(strcmp(argv[i], "-list") == 0)
             {
@@ -1363,22 +1419,24 @@ int main(int argc, char **argv)
                         printf("Failed to open audio\n");
                 }
             } 
-            if (startgame > 1)
+            loadHighScores();
+            loadGameOverlays();
+            if (startgame[0] != 0)
             {
-                int tmp = startgame;
-                startgame = -1;
+                printf("Start Game: %s", startgame);
+                bool found = false;
                 for (int i = 0; i < gameCount; i++)
                 {
-                    if(strcmp(argv[tmp], getGame(i).title) == 0)
+                    if(strcmp(startgame, getGame(i).title) == 0)
                     {
-                        startgame = i;
+                        found = true;
                         restartGame(i);
                         break;
                     }
                 }
-            }
-            loadHighScores();
-            loadGameOverlays();
+                if(!found)
+                    memset(startgame, 0, 100);
+            }            
             int skip = 10;
             while(quit == 0)
             {
@@ -1427,12 +1485,17 @@ int main(int argc, char **argv)
 			    SDL_FreeSurface(screen);
             saveHighScores();
             saveGameOverlays();
+            if(soundOn)
+            {
+                SDL_PauseAudio(1);
+                SDL_CloseAudio();
+                SDL_QuitSubSystem(SDL_INIT_AUDIO);
+            }
 		}
 		else
 		{
 			printf("Failed to Set Videomode %dx%d\n",WINDOW_WIDTH, WINDOW_HEIGHT);
 		}
-		SDL_Quit();
 	}
 	else
 	{
